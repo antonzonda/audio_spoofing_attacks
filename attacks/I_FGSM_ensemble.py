0d38abe25aef9e3edafea5326abcd4653e260ba2
antonzonda <zhouxt1@shanghaitech.edu.cn>
2022-12-09 19:19:10 +0800

    Add I-FGSM

---
+++ attacks/I_FGSM_ensemble.py
@@ -0,0 +1,66 @@
+from torch import nn
+import torch
+
+from attacks.loss import get_loss_fn
+
+# iterative FGSM ensemble attack
+class I_FGSM_ensemble():
+    def __init__(self, models, attack_config) -> None:
+        
+        self.loss_fn = get_loss_fn( attack_config['loss'] )
+        self.epsilon = attack_config['epsilon']
+
+        self.models = models
+        self.num_models = len(models)
+
+        self.max_iter = attack_config['max_iter']
+        # self.alpha = attack_config['alpha']
+        self.alpha = self.epsilon / self.max_iter
+        self.device = "cuda"
+
+    def attack(self, x, y):
+
+        x = x.clone() # avoid influencing
+        # x.requires_grad = True
+
+        # x.requires_grad = True
+
+        x = x.clone().detach().to(self.device)
+        y = y.clone().detach().to(self.device)
+
+        g = torch.zeros_like(x).to(self.device)
+        adv_x = x.clone().detach()
+
+        for t in range(self.max_iter):
+            # print(t)
+            adv_x.requires_grad = True
+
+            out_list = []
+            for model in self.models:
+                model.eval()
+                _, out = model(adv_x)
+                out_list.append(out)
+            
+            # print(out_list[0].size())
+
+            out_mean = torch.mean(torch.stack(out_list), dim=0)
+
+            loss = self.loss_fn(out_mean, y)
+
+            # self.model.zero_grad()
+            # Calculate gradients of model in backward pass
+            # loss.backward()
+            
+            # Collect datagrad
+            data_grad = torch.autograd.grad(loss, adv_x,
+                                       retain_graph=False, create_graph=False)[0]
+
+            # x.grad.zero_()
+            # Create the adversarial audio
+            adv_x = adv_x.detach() + self.alpha * data_grad.sign()
+
+            # we need to clamp the data in (-1, 1)
+            delta = torch.clamp(adv_x - x, min=-self.epsilon, max=self.epsilon)
+            adv_x = torch.clamp(adv_x, min=-(1-2**(-15)), max=1-2**(-15)).detach()
+
+        return adv_x
