41fefef9cf16503d2d9edb4f8ea966bb1e320497
Golfer Chen <47413995+GolferChen@users.noreply.github.com>
2023-01-12 11:10:12 +0800

    Create Noise.py

---
+++ attacks/Noise.py
@@ -0,0 +1,17 @@
+
+# preparation:
+# 1. install speechbrain
+# 2. 定位speechbrain的源码，把lobes/features.py和lobes/augment.py中的torch.no_grad()删掉
+
+class NOISE(nn.Module):
+
+    # def __init__(self, csv_file=None, sorting="random", snr_low=15, snr_high=15, pad=False) -> None:
+    def __init__(self, csv_file=None, sorting="random", snr_low=0, snr_high=5, pad=False) -> None:
+        super().__init__()
+
+        self.noise_apply = AddNoise(csv_file=csv_file, sorting=sorting, snr_low=snr_low, snr_high=snr_high, pad_noise=pad)
+    
+    def forward(self, waveforms):
+        # waveforms： shape: (Batch, 1, Time)
+        lengths = torch.ones(waveforms.shape[0], device=waveforms.device, dtype=torch.float)
+        return self.noise_apply(waveforms.squeeze(1), lengths).unsqueeze(1)
