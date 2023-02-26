9dec08fbf8b592164f1b6fc5c3b7ebed882c395d
Golfer Chen <47413995+GolferChen@users.noreply.github.com>
2023-02-26 22:20:06 +0800

    Update Noise.py

--- attacks/Noise.py
+++ attacks/Noise.py
@@ -18,3 +18,29 @@ class NOISE(nn.Module):
         # waveforms： shape: (Batch, 1, Time)
         lengths = torch.ones(waveforms.shape[0], device=waveforms.device, dtype=torch.float)
         return self.noise_apply(waveforms.squeeze(1), lengths)
+
+ 
+ class DropC(nn.Module): # random
+        
+    # def __init__(self, drop_length_low=5000, drop_length_high=5000, drop_count_low=4, drop_count_high=5) -> None:
+    def __init__(self, drop_length_low=2000, drop_length_high=4000, drop_count_low=0, drop_count_high=5) -> None:
+        super().__init__()
+
+        drop_start = 0
+        drop_end = None
+
+        drop_prob = 1
+        
+        # noise_factor = 0.0
+        drop_chunk_noise_factor = 0
+        noise_factor = drop_chunk_noise_factor
+
+        self.drop_chunk_instance = DropChunk(drop_length_low=drop_length_low, drop_length_high=drop_length_high,
+            drop_count_low=drop_count_low, drop_count_high=drop_count_high,
+            drop_start=drop_start, drop_end=drop_end, drop_prob=drop_prob,
+            noise_factor=noise_factor)
+    
+    def forward(self, audios, length=None):
+        length = length if length is not None else \
+        torch.tensor([1.0] * audios.shape[0], device=audios.device, dtype=torch.long)
+        audios = audios.squeeze(1)
